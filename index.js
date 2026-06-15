37d5c8ce41cb0cee0c4b0fb8fdddb423e20db0fa
victoria <v.suprun1961@gmail.com>
2026-06-15 09:37:54 +0300

    fix: fix styles and layout (понедельник, июня 15, 2026 09:37:54)

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("JS работает!");
+
+alert("JS подключен!");
+
+console.log(
+  "All points of the technical specifications\n  have been fulfilled \n Main page: Markup validation - +10 \n Layout matches the design - +35\n CSS requirements - +15; \n Interactivity - +10 \n. Pets page: Markup validation - +10 \n Layout matches the design - +15 \n CSS requirements - +5 \n Interactivity - +10",
+);
